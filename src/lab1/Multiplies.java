ce5d7ac3825cf0e1d5feed69c93cc39f50131ef5
CandiceJin1204 <jinzirong2024@163.com>
2025-09-09 16:19:44 -0400

    refactored to extract helper method

--- src/lab1/Multiplies.java
+++ src/lab1/Multiplies.java
@@ -2,6 +2,12 @@ package lab1;
 
 public class Multiplies {
     public static void main(String[] args) {
-
+        int result = 0;
+        for (int i = 0; i < 1000; i ++){
+            if(i % 3 == 0 | i % 5 == 0){
+                result += 1;
+            }
+        }
+        System.out.println(result);
     }
 }
